c4b8c9e631ca44ee92e6a9198f1cfb08a8dd7b4a
lewis <lewislcc@163.com>
2019-03-14 18:21:24 +0800

    adjust doc structure

--- main.py
+++
@@ -1,8 +0,0 @@
-from log_check import LogCheck
-
-def test():
-    lc = LogCheck()
-    lc.checkLog()
-
-if __name__ == '__main__':
-    test()
--- myconfigparser.py
+++
@@ -1,7 +0,0 @@
-import configparser
-
-class MyConfigParser(configparser.ConfigParser):
-    def __init__(self,defaults=None):
-        configparser.ConfigParser.__init__(self,defaults=None)
-    def optionxform(self, optionstr):
-        return optionstr
