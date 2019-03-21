4b8221af50c1c180680c8537cb782464f56b86fb
lewis32 <lewislcc@163.com>
2019-03-21 23:51:13 +0800

    initial version

--- core/log_check.py
+++ core/log_check.py
@@ -22,9 +22,6 @@ class LogCheck():
             return loglist
 
     def loadPolicy(self):
-        # dirpath = os.getcwd()
-        # filepath = os.path.join(dirpath,'policy.ini')
-        # conf = configparser.ConfigParser()
         conf = myconfigparser.MyConfigParser()
         conf.read(self.filepath+r'\\conf\\policy.ini',encoding='utf-8')
         return conf
@@ -35,20 +32,14 @@ class LogCheck():
         with open(self.filepath+r'\\result\\result-'+self.stime+'.txt','w') as f:
             for log in loglist:
                 if log['EventCode'] not in conf.sections():
-                    # print('Undefined EventCode:',log['EventCode'])
                     f.writelines(['Undefined EventCode:',log['EventCode'],'\n'])
-                    # return 0
                 for key in log:
                     if key not in conf.options('basic'):
                         if key not in conf.options(log['EventCode']):
                             f.writelines(['Undefined key:',key,'\n'])
                             continue
                         elif not re.match(eval(conf.get(log['EventCode'],key)),log[key]):
-                            # f.writelines(['Passed key-value:',key,log[key],'\n'])
-                        # else:
                             f.writelines(['Wrong key-value:',key,log[key],'\n'])
                     elif not re.match(eval(conf.get('basic',key)),log[key]):
-                        # f.writelines(['Passed key-value:',key,log[key],'\n'])
-                    # else:
                         f.writelines(['Wrong key-value:',key,log[key],'\n'])
                 f.writelines(['*******************************\n'])
