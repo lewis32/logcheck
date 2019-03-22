eec462523e3480292411e90365616527c86ed203
lewis <lewislcc@163.com>
2019-03-22 18:21:48 +0800

    initial version

--- core/log_check.py
+++ core/log_check.py
@@ -3,8 +3,9 @@ import re
 import json
 import sys
 import time
-sys.path.append(os.path.abspath(os.getcwd()+'\\package'))
-import myconfigparser
+import bisect
+# sys.path.append(os.path.abspath(os.getcwd()+'\\package'))
+import package.myconfigparser
 
 class LogCheck():
     filepath = os.path.abspath((os.path.dirname(os.getcwd())))
@@ -22,12 +23,31 @@ class LogCheck():
             return loglist
 
     def loadPolicy(self):
-        # dirpath = os.getcwd()
-        # filepath = os.path.join(dirpath,'policy.ini')
-        # conf = configparser.ConfigParser()
         conf = myconfigparser.MyConfigParser()
         conf.read(self.filepath+r'\\conf\\policy.ini',encoding='utf-8')
         return conf
+
+    def compareKey(self):
+        loglist = self.loadLog()
+        conf = self.loadPolicy()
+        with open(self.filepath+r'\\result\\result-'+self.stime+'.txt','w') as f:
+            samedatalist = []
+            for log in loglist:
+                samedata = []
+                exlogdata = []
+                exconfdata = []
+                log = list(log)
+                for i in log:
+                    pos = bisect.bisect_left(conf,i)
+                    if pos < len(conf) and conf[pos] == i:
+                        samedata.append(i)
+                    elif pos >= len(conf):
+                        exlogdata.append(i)
+                samedata = list(set(samedata))
+                print(samedata)
+
+
+
 
     def checkLog(self):
         loglist = self.loadLog()
@@ -35,20 +55,14 @@ class LogCheck():
         with open(self.filepath+r'\\result\\result-'+self.stime+'.txt','w') as f:
             for log in loglist:
                 if log['EventCode'] not in conf.sections():
-                    # print('Undefined EventCode:',log['EventCode'])
                     f.writelines(['Undefined EventCode:',log['EventCode'],'\n'])
-                    # return 0
                 for key in log:
                     if key not in conf.options('basic'):
                         if key not in conf.options(log['EventCode']):
                             f.writelines(['Undefined key:',key,'\n'])
                             continue
                         elif not re.match(eval(conf.get(log['EventCode'],key)),log[key]):
-                            # f.writelines(['Passed key-value:',key,log[key],'\n'])
-                        # else:
                             f.writelines(['Wrong key-value:',key,log[key],'\n'])
                     elif not re.match(eval(conf.get('basic',key)),log[key]):
-                        # f.writelines(['Passed key-value:',key,log[key],'\n'])
-                    # else:
                         f.writelines(['Wrong key-value:',key,log[key],'\n'])
                 f.writelines(['*******************************\n'])
