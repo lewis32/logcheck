6d93c314bf8f83443b444468a25eadfbe9806e02
lewis <lewislcc@163.com>
2019-03-25 18:32:45 +0800

    initail version

--- core/log_check.py
+++ core/log_check.py
@@ -8,6 +8,7 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__))+'\\package')
 import myconfigparser
 
 class LogCheck():
+
     filepath = os.path.abspath((os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
     ltime = time.localtime()
     stime = time.strftime('%Y%m%d-%H%M%S', ltime)
@@ -28,39 +29,52 @@ class LogCheck():
         return conf
 
     def compareKeys(self,log,conf):
-        samedata = []
-        exlogdata = []
-        exconfdata = []
+        mutual_key = []
+        ex_key_log = []
+        ex_key_conf = []
         log = list(log)
         conf = list(conf)
         conf.sort()
         for i in log:
             pos = bisect.bisect_left(conf,i)
+            print(pos,len(conf))
             if pos < len(conf) and conf[pos] == i:
-                samedata.append(i)
+                mutual_key.append(i)
             elif pos >= len(conf):
-                exlogdata.append(i)
-        samedata = list(set(samedata))
-        exlogdate = list(set(exlogdata))
-        print(samedata)
-        print(exlogdata)
+                ex_key_log.append(i)
+        mutual_key = list(set(mutual_key))
+        ex_key_log = list(set(ex_key_log))
+        ex_key_conf = list(set(conf).difference(set(mutual_key)))
+        return mutual_key,ex_key_log,ex_key_conf
 
     def checkLog(self):
         loglist = self.loadLog()
         conf = self.loadPolicy()
-        with open(self.filepath+r'\\result\\result-'+self.stime+'.txt','w') as f:
+        with open(self.filepath+r'\\result\\result-'+self.stime+'.txt','w',encoding='utf-8' ) as f:
             for log in loglist:
                 if log['EventCode'] not in conf.sections():
                     f.writelines(['Undefined EventCode:',log['EventCode'],'\n'])
                 else:
-                    self.compareKeys(log,conf.options('basic')+conf.options(log['EventCode']))
+                    mutual_key,ex_key_log,ex_key_conf = self.compareKeys(log,conf.options('basic')+conf.options(log['EventCode']))
+                    if len(ex_key_log) != 0:
+                        f.writelines(['Undefined key-value: '])
+                        for i in ex_key_log:
+                            f.writelines([i])
+                        f.writelines(['\n'])
+                    elif len(ex_key_conf) != 0:
+                        f.writelines(['Lacking key-value: '])
+                        for i in ex_key_conf:
+                            f.writelines([i])
+                        f.writelines(['\n'])
+                    else:
+                        f.writelines(['log keys is the same as conf keys'])
                 for key in log:
                     if key not in conf.options('basic'):
                         if key not in conf.options(log['EventCode']):
-                            f.writelines(['Undefined key:',key,'\n'])
+                            # f.writelines(['Undefined key:',key,'\n'])
                             continue
                         elif not re.match(eval(conf.get(log['EventCode'],key)),log[key]):
-                            f.writelines(['Wrong key-value:',key,log[key],'\n'])
+                            f.writelines(['Wrong key-value: ',key,':',log[key],'\n'])
                     elif not re.match(eval(conf.get('basic',key)),log[key]):
-                        f.writelines(['Wrong key-value:',key,log[key],'\n'])
-                f.writelines(['*******************************\n'])
+                        f.writelines(['Wrong key-value: ',key,':',log[key],'\n'])
+                f.writelines(['*******************下一条日志*******************\n'])
