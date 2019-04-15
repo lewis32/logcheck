26421f1af7a36dd0fd3fd9a8963da6b9e03d4313
lewis <lewislcc@163.com>
2019-04-15 19:21:10 +0800

    initial version

--- core/log_check.py
+++ core/log_check.py
@@ -28,7 +28,7 @@ class LogCheck():
         conf.read(self.filepath+r'\\conf\\policy.ini',encoding='utf-8')
         return conf
 
-    def compareKeys(self,log,conf,f):
+    def compareKeys(self,log,conf):
         mutual_key = []
         mutual_key_lower = []
         log = [i for i in log]
@@ -43,14 +43,9 @@ class LogCheck():
         mutual_key_lower = list(set(mutual_key_lower))
         log_key = list(set(log).difference(set(mutual_key)))
         conf_key = list(set(conf).difference(set(mutual_key_lower)))
-        print(log,mutual_key,)
-        if len(log_key) != 0:
-            f.writelines(['日志存在多余Key: ',str(log_key),'\n'])
-        if len(conf_key) != 0:
-            f.writelines(['日志存在缺少Key: ',str(conf_key),'\n'])
-        return mutual_key
+        return mutual_key,log_key,conf_key
 
-    def compareTimestamp(self,log,f):
+    def compareTimestamp(self,log):
         st = 1
         et = 2
         for key in log:
@@ -58,11 +53,8 @@ class LogCheck():
                 st = log[key]
             if self.lowerKeys(key) == 'endtime':
                 et = log[key]
-        if st == '0':
-            f.writelines(['Wrong Key-Value: StartTime == 0'])
-        elif et != '0' and st >= et:
-            print(log)
-            f.writelines(['Wrong Key-Value: StartTime >= EndTime',st,et])
+        if et != '0' and st >= et:
+            return 1
 
     def lowerKeys(self,key):
         return key.lower()
@@ -70,42 +62,48 @@ class LogCheck():
     def checkLog(self):
         try:
             loglist = self.loadLog()
-        except Exception as e:
-            print("日志格式不规范: ",e)
-
-        try:
             conflist = self.loadPolicy()
         except Exception as e:
-            print("策略格式不规范: ",e)
-
-        with open(self.filepath+r'\\result\\result-'+self.stime+'.txt','w',encoding='utf-8' ) as f:
-            for log in loglist:
-                count = len(log)
-                n = 1
-                for key in log:
-                    if self.lowerKeys(key) != 'eventcode' and n < count:
-                        n += 1
-                        continue
-                    elif self.lowerKeys(key) != 'eventcode' and n == count:
-                        f.writelines(['Couldn\'t find Key: eventcode\n'])
-                        break
-                    else:
-                        try:
-                            conf = dict(conflist.items(log[key]) + conflist.items('common'))
-                        except Exception as e:
-                            print("日志eventcode不规范: ",e)
+            print("Failed to load log or policy: ",e)
+        else:
+            with open(self.filepath+r'\\result\\result-'+self.stime+'.txt','w',encoding='utf-8' ) as f:
+                for log in loglist:
+                    count = len(log)
+                    n = 1
+                    f.writelines(['****************** Begin ******************\n'])
+                    for key in log:
+                        if self.lowerKeys(key) != 'eventcode' and n < count:
+                            n += 1
+                            continue
+                        elif self.lowerKeys(key) != 'eventcode' and n == count:
+                            f.writelines(['Can\'t find key: eventcode\n'])
+                            break
                         else:
-                            for i in conf:
-                                conf[self.lowerKeys(i)] = conf.pop(i)
-                            mutual_key = self.compareKeys(log,conf,f)
-                            mutual_dict = {}
-                            for i in mutual_key:
-                                mutual_dict[i] = conf[self.lowerKeys(i)]
-                            for key in mutual_dict:
-                                if not re.match(eval(mutual_dict[key]),str(log[key])):
-                                    f.writelines(['Wrong Key-Value: ',key,':',log[key],'\n'])
-                            self.compareTimestamp(log,f)
-                f.writelines(['\n******************* NEXT LOG *******************\n\n'])
+                            try:
+                                conf = dict(conflist.items(log[key]) + conflist.items('common'))
+                            except Exception as e:
+                                print("Failed to combine common keys with event keys : ",e)
+                            else:
+                                f.writelines(['Eventcode: ',log[key],'\n'])
+                                for i in conf:
+                                    conf[self.lowerKeys(i)] = conf.pop(i)
+                                mutual_key,log_key,conf_key = self.compareKeys(log,conf)
+                                if len(log_key) != 0:
+                                    f.writelines(['Extra key in log: ',str(log_key),'\n'])
+                                if len(conf_key) != 0:
+                                    f.writelines(['Missing key in log: ',str(conf_key),'\n'])
+                                mutual_dict = {}
+                                invalid_mutual_dict = {}
+                                for i in mutual_key:
+                                    mutual_dict[i] = conf[self.lowerKeys(i)]
+                                for mutual_key in mutual_dict:
+                                    if not re.match(eval(mutual_dict[mutual_key]),str(log[key])):
+                                        invalid_mutual_dict[key] = log[key]
+                                        # f.writelines(['Key-Value is invalid: ',key,':',log[key],'\n'])
+                                if self.compareTimestamp(log) == 1:
+                                    f.writelines(['Key-value is invalid: StartTime >= EndTime\n'])
+                    f.writelines(['Key-Value is invalid: ',str(invalid_mutual_dict),'\n'])
+                    f.writelines(['******************* End *******************\n\n'])
 
 
 
